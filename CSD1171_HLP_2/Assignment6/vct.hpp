46bfb00be71015eb0e9d8eeefd291991c3c5c67c
Edgar <32952693+Yongyiphan@users.noreply.github.com>
2023-03-09 01:57:13 +0800

    BS code. hard stuck Ass6

--- CSD1171_HLP_2/Assignment6/vct.hpp
+++ CSD1171_HLP_2/Assignment6/vct.hpp
@@ -3,14 +3,15 @@
 #define VCT_HPP
 ////////////////////////////////////////////////////////////////////////////////
 #include "initializer_list"
-
-namespace hlp2 {
+#include <cstdlib>
 
 // provide definition of class template vector:
 // 1) Start with the correct implementation of assignment 6.
 // 3) Add member function: void swap(vector&);
 // 4) Rewrite [if necessary] copy assignment operator overloads to follow
 //    the copy-and-swap idiom.
+namespace hlp2 {
+
 // 5) Add member function: void pop_back();
 //    What does pop_back() do? It removes the last element in the vector, effectively reducing the container size by one.
 // 6) Use your own tests to make sure your class does the right thing for
@@ -27,54 +28,175 @@ template <typename T>
 class vector {
 public:
   // types to declare: size_type, value_type,
-  using size_type = size_t;
-  using value_type = T;
+  using size_type       = size_t;
+  using value_type      = T;
   // reference, const_reference, pointer, const_pointer
-  using reference = value_type&;
-  using const_reference = const reference;
-  using pointer = value_type*;
-  using const_pointer = const value_type*;
+  using reference       = value_type&;
+  using const_reference = const value_type&;
+  using pointer         = value_type*;
+  using const_pointer   = const value_type*;
 
 public:
   vector();
-  explicit vector(size_type n);
-  vector(std::initializer_list<int> rhs);
-  vector(std::initializer_list<T> rhs);
-  vector(vector<T> const&rhs);
+  vector(vector<T> const &rhs);
+  vector(std::initializer_list<T> const& rhs);
   ~vector();
   
-  vector& operator=(vector const& rhs);
-  vector& operator=(std::initializer_list<int> rhs);
-  vector& operator=(std::initializer_list<T> rhs);
-  reference operator[](size_type i );
-  const_reference operator[](size_type i) const;
+  vector&         operator=(vector const& rhs);
+  vector&         operator=(std::initializer_list<T> const& rhs);
+  reference       operator[](size_type i )     ; 
+  const_reference operator[](size_type i) const; 
   
-  void push_back(value_type value);
+  void push_back(T);
   void pop_back();
-  void reserve(size_type newsize);
-  void resize(size_type newsize);
-  void swap(const vector<T>&);
+  void reserve(size_type n);
+  void resize(size_type n);
+  void swap(vector&rhs);
   
-  bool empty() const;
-  size_type size() const;
-  size_type capacity() const;
+  bool      empty()       const;
+  size_type size()        const;
+  size_type capacity()    const;
   size_type allocations() const;
   
-  pointer begin();
-  pointer end();
-  const_pointer begin() const;
-  const_pointer end() const;
-  const_pointer cbegin() const;
-  const_pointer cend() const;
+  pointer       begin()        ;
+  const_pointer begin()   const;
+  const_pointer cbegin()  const;
+  pointer       end()          ;
+  const_pointer end()     const;
+  const_pointer cend()    const;
   
 
 private:
-  size_type sz;     // the number of elements in the array
-  size_type space;  // the allocated size (in terms of elements) of the array
-  size_type allocs; // number of times space has been updated
-  pointer   data;   // the dynamically allocated array (int*)
+  size_type sz{0};     // the number of elements in the array
+  size_type space{0};  // the allocated size (in terms of elements) of the array
+  size_type allocs{0}; // number of times space has been updated
+  pointer   data{nullptr};   // the dynamically allocated array (int*)
 };
 
-} // namespace hlp2
+template <class T>
+vector<T>::vector(){}
+//template <class T>
+template <class T>
+vector<T>::vector(vector<T> const &rhs){
+  reserve(static_cast<size_type>(rhs.size()));
+  for(const T&i:rhs){push_back(i);}
+}
+template <class T>
+vector<T>::vector(std::initializer_list<T> const& rhs){
+  reserve(static_cast<size_type>(rhs.size()));
+  for(const T&i:rhs){push_back(i);}
+}
+template <class T>
+vector<T>::~vector(){if(data)std::free(data);}
+
+
+template <class T>
+vector<T>&          vector<T>::operator=(vector const& rhs){
+  vector temp{rhs};
+  swap(temp);
+  return *this;
+}
+template <class T>
+vector<T>&          vector<T>::operator=(std::initializer_list<T>const& rhs){
+  vector temp{rhs};
+  swap(temp);
+  return *this;
+}
+
+template <class T>
+void vector<T>::pop_back(){sz--;}
+
+template <class T>
+void vector<T>::push_back(T value){
+    if(space == 0){
+        reserve(static_cast<size_type>(1));
+    }
+    else if(sz==space){
+        reserve(static_cast<size_type>(space * 2));
+    }
+    *(data + sz) = value;
+    sz++;
+}
+
+
+template <class T>
+void vector<T>::reserve(size_type n){
+    if(n <= space)
+        return;
+    pointer na = (pointer)std::malloc(sizeof(T) * n);
+    allocs++;
+    if(data){
+        for(size_type i{}; i < sz; na[i] = data[i], i++);
+        std::free(data);
+    }
+    data = na;
+    space = n;
+}
+
+template <class T>
+void vector<T>::resize(size_type n){
+    if(n>space){
+      reserve(n);
+      sz = n;
+    }
+    else if(n>sz && n <= space){
+      sz = n;
+    }
+    else if (n < sz){
+      sz = n;
+    }
+    else if (n == sz){
+        return;
+    }
+    
+
+}
+
+template <class T>
+void vector<T>::swap(vector& rhs){
+  pointer   tdata   = rhs.data;
+  size_type tsz     = rhs.size();
+  size_type tspace  = rhs.capacity();
+  size_type tallocs = rhs.allocations();
+
+  rhs.data    = data;
+  rhs.sz      = sz;
+  rhs.space   = space;
+  rhs.allocs  = allocs;
+
+  data    = tdata;
+  sz      = tsz;
+  space   = tspace;
+  allocs  = tallocs;
+
+}
+
+
+template <class T>
+typename vector<T>::reference         vector<T>::operator[](size_type i )      {return *(data + i);}
+template <class T>
+typename vector<T>::const_reference   vector<T>::operator[](size_type i) const {return *(data + i);}
+template <class T>
+bool      vector<T>::empty()                           const{return sz?false:true;}
+template <class T>
+typename vector<T>::size_type vector<T>::size()        const{return sz;}
+template <class T>
+typename vector<T>::size_type vector<T>::capacity()    const{return space;}
+template <class T>
+typename vector<T>::size_type vector<T>::allocations() const{return allocs;}
+template <class T>
+typename vector<T>::pointer       vector<T>::begin()        {return data;}
+template <class T>
+typename vector<T>::const_pointer vector<T>::begin()   const{return data;}
+template <class T>
+typename vector<T>::const_pointer vector<T>::cbegin()  const{return data;}
+template <class T>
+typename vector<T>::pointer       vector<T>::end()          {return data + sz;}
+template <class T>
+typename vector<T>::const_pointer vector<T>::end()     const{return data + sz;}
+template <class T>
+typename vector<T>::const_pointer vector<T>::cend()    const{return data + sz;}
+
+}
 #endif // VCT_HPP
 
